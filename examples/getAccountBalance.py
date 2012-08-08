353bac5df1c2173ac58b493e55d0f1ee9921ed4a
Alejandro Wainzinger <alex.wainzinger@gengo.com>
2012-08-08 04:07:36 +0000

    GG-979 Remove hardcoded keys from examples.

--- examples/getAccountBalance.py
+++ examples/getAccountBalance.py
@@ -5,8 +5,8 @@ from mygengo import MyGengo
 
 # Get an instance of MyGengo to work with...
 gengo = MyGengo(
-    public_key = 'xpU@jqEzqnXCb#OOsAeR4z49IX|j}#dwyliMp2RIq1vM9OIKq-K#{mg~sVBUX^91',
-    private_key = '~Q9hI|sV(I^iX7|8WQ=l5=CvUmEWx3[=c5ms09|$JIuT-$aiTIYkS4~1F7^C9dw3',
+    public_key = 'your_public_key',
+    private_key = 'your_private_key'
     sandbox = False, # possibly false, depending on your dev needs
 	debug = True
 )
