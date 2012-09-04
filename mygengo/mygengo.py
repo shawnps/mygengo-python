96d5dff4d8914fd419a7b63b2c42d724c8b1fd48
Florian v. Bock <florian@mygengo.com>
2012-09-04 07:40:18 +0000

    GG-979 switch to using requests

--- mygengo/mygengo.py
+++ mygengo/mygengo.py
@@ -12,12 +12,15 @@ __author__ = 'Alex Wainzinger <alex.wainzinger@gengo.com>'
 __version__ = '1.3.3'
 
 import httplib2, mimetypes, mimetools, re, hmac
+import requests
 
 from pprint import pprint
 from hashlib import sha1
 from urllib import urlencode, quote
 from time import time
 from operator import itemgetter
+from string import lower
+from pprint import pprint
 
 # mockdb is a file with a dictionary of every API endpoint for Gengo.
 from mockdb import api_urls, apihash
@@ -55,7 +58,7 @@ class MyGengoError(Exception):
 			# Auth errors tend to be the most requested for their own
 			# Exception instances, so give it to the masses, yo.
 			raise MyGengoAuthError(msg)
-	
+
 	def __str__(self):
 		return repr(self.msg)
 
@@ -66,7 +69,7 @@ class MyGengoAuthError(MyGengoError):
 	"""
 	def __init__(self, msg):
 		self.msg = msg
-	
+
 	def __str__(self):
 		return repr(self.msg)
 
@@ -75,7 +78,7 @@ class MyGengo(object):
 		"""MyGengo(public_key = None, private_key = None, sandbox = False, headers = None)
 
 			Instantiates an instance of MyGengo.
-			
+
 			Parameters:
 				public_key - Your 'public' key for Gengo. Retrieve this from your account information if you want to do authenticated calls.
 				private_key - Your 'private' key for Gengo. Retrieve this from your account information if you want to do authenticated calls.
@@ -96,8 +99,8 @@ class MyGengo(object):
 		# No matter whether we get some supplied or use the generic, tell it we want JSON. ;P
 		self.headers['Accept'] = 'application/json'
 		self.debug = debug
-		self.client = httplib2.Http()
-	
+		# self.client = httplib2.Http()
+
 	def __getattr__(self, api_call):
 		"""
 			The most magically awesome block of code you'll ever see.
@@ -120,7 +123,7 @@ class MyGengo(object):
 		def get(self, **kwargs):
 			# Grab the (hopefully) existing method 'definition' to fire off from our api hash table.
 			fn = apihash[api_call]
-			
+
 			# Do a check here for specific job sets - we need to support posting multiple jobs
 			# at once, so see if there's an dictionary of jobs passed in, pop it out, let things go on as normal,
 			# then pick this chain back up below...
@@ -130,15 +133,15 @@ class MyGengo(object):
 			if 'comment' in kwargs: post_data['comment'] = kwargs.pop('comment')
 			if 'action' in kwargs: post_data['action'] = kwargs.pop('action')
 			if 'job_ids' in kwargs: post_data['job_ids'] = kwargs.pop('job_ids')
-			
+
 			# This is for the one-off GET call that acts like a POST call... don't ask. ;P
 			if 'ids' in kwargs:
 				kwargs['ids'] = '/%s' % ','.join(map(lambda i: str(i), ids))
-			
+
 			# Set up a true base URL, abstracting away the need to care about the sandbox mode
 			# or API versioning at this stage.
 			base_url = self.api_url.replace('{{version}}', 'v%s' % self.api_version)
-			
+
 			# Go through and replace any mustaches that are in our API url with their appropriate key/value pairs...
 			# NOTE: We pop() here because we don't want the extra data included and messing up our hash down the road.
 			base = re.sub(
@@ -146,7 +149,7 @@ class MyGengo(object):
 				lambda m: "%s" % kwargs.pop(m.group(1), 'no_argument_specified'), # In case of debugging needs
 				base_url + fn['url']
 			)
-			
+
 			# Build up a proper 'authenticated' url...
 			#
 			# Note: for further information on what's going on here, it's best to familiarize yourself
@@ -155,24 +158,25 @@ class MyGengo(object):
 			if self.public_key is not None:
 				query_params['api_key'] = self.public_key
 			query_params['ts'] = str(int(time()))
-			
+
 			# If any further APIs require their own special signing needs, fork here...
                         # For now, we are supporting 1.1 only, but 2 is desired at some point.
-			resp, content = self.signAndRequestAPILatest(fn, base, query_params, post_data)
-			results = json.loads(content)
-			
+			# resp, content = self.signAndRequestAPILatest(fn, base, query_params, post_data)
+			# results = json.loads(content)
+			results = self.signAndRequestAPILatest(fn, base, query_params, post_data).json
+
 			# See if we got any weird or odd errors back that we can cleanly raise on or something...
 			if 'opstat' in results and results['opstat'] != 'ok':
 				raise MyGengoError(results['err']['msg'], results['err']['code'])
-			
+
 			# If not, screw it, return the junks!
 			return results
-			
+
 		if api_call in apihash:
 			return get.__get__(self)
 		else:
 			raise AttributeError
-	
+
 	def signAndRequestAPILatest(self, fn, base, query_params, post_data = {}):
 		"""
 			Request signatures between API v1 and later versions of the API differ greatly in
@@ -189,6 +193,7 @@ class MyGengo(object):
 		# Encoding jobs becomes a bit different than any other method call, so we catch them and do a little
 		# JSON-dumping action. Catching them also allows us to provide some sense of portability between the various
 		# job-posting methods in that they can all safely rely on passing dictionaries around. Huzzah!
+		req_method = requests.__getattribute__( lower( fn['method'] ) )
 		if fn['method'] == 'POST' or fn['method'] == 'PUT':
 			if 'job' in post_data:
 				query_params['data'] = json.dumps(post_data['job'], separators = (',', ':'))
@@ -198,28 +203,34 @@ class MyGengo(object):
 				query_params['data'] = json.dumps(post_data['comment'], separators = (',', ':'))
 			elif 'action' in post_data:
 				query_params['data'] = json.dumps(post_data['action'], separators = (',', ':'))
-			
+
+			pprint( post_data )
+
 			query_json = json.dumps(query_params, separators = (',', ':'), sort_keys = True).replace('/', '\\/')
 			query_hmac = hmac.new(self.private_key, query_params['ts'], sha1)
 			query_params['api_sig'] = query_hmac.hexdigest()
 			query_data = urlencode(query_params)
-			
+
 			# Httplib2 doesn't assume this for POST requests, but in the case of the Gengo API it's a bit of a hidden necessity.
 			headers = self.headers
 			headers['Content-Type'] = 'application/x-www-form-urlencoded'
-			
+
 			if self.debug is True: print query_data
-			return self.client.request(base, fn['method'], headers = headers, body = query_data)
+			return req_method( base, headers = headers, data = query_data )
 		else:
 			query_string = urlencode(sorted(query_params.items(), key = itemgetter(0)))
 			if self.private_key is not None:
 				query_hmac = hmac.new(self.private_key, query_params['ts'], sha1)
 				query_params['api_sig'] = query_hmac.hexdigest()
 				query_string = urlencode(query_params)
-			
+
 			if self.debug is True: print query_string
-			return self.client.request(base + '?%s' % query_string, fn['method'], headers = self.headers)
-	
+			# return self.client.request(base + '?%s' % query_string, fn['method'], headers = self.headers)
+			return req_method( base + '?%s' % query_string, headers = self.headers )
+
+	def upload_file( self ):
+            pass
+
 	@staticmethod
 	def unicode2utf8(text):
 		try:
