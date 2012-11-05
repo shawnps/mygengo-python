f68f4db8f1908ca68050a66134c39a91034267ca
Shawn Smith <shawnpsmith@gmail.com>
2012-11-05 15:03:32 +0900

    more tabs to spaces

--- mygengo/test_keys_example.py
+++ mygengo/test_keys_example.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 """
-	This file mainly exists for storing your public/private
-	keypair in a location that's not tracked through Git. To
-	run the Gengo test suite, fill in the values below and copy
-	this file over to test_keys.py.
+    This file mainly exists for storing your public/private
+    keypair in a location that's not tracked through Git. To
+    run the Gengo test suite, fill in the values below and copy
+    this file over to test_keys.py.
 
-	e.g: cp test_keys_example.py test_keys.py
+    e.g: cp test_keys_example.py test_keys.py
 """
 
 public_key = ''
