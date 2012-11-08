7f8518953e2c27b982aa0f1d183fffa323119e98
Shawn Smith <shawnpsmith@gmail.com>
2012-11-08 17:15:06 +0900

    pep8 and add license header to all python files

--- setup.py
+++ setup.py
@@ -1,6 +1,38 @@
-#!/usr/bin/python
+# All code provided from the http://gengo.com site, such as API example code
+# and libraries, is provided under the New BSD license unless otherwise
+# noted. Details are below.
+#
+# New BSD License
+# Copyright (c) 2009-2012, myGengo, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+# Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+# Neither the name of myGengo, Inc. nor the names of its contributors may
+# be used to endorse or promote products derived from this software
+# without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import sys, os
+import sys
+import os
 from distutils.core import Command
 from setuptools import setup
 from setuptools import find_packages
@@ -10,6 +42,7 @@ __version__ = '1.3.4'
 
 # Command based on Libcloud setup.py:
 # https://github.com/apache/libcloud/blob/trunk/setup.py
+
 
 class Pep8Command(Command):
     description = "Run pep8 script"
@@ -36,28 +69,28 @@ class Pep8Command(Command):
 
 setup(
     # Basic package information.
-    name = 'mygengo',
-    version = __version__,
-    packages = find_packages(),
+    name='mygengo',
+    version=__version__,
+    packages=find_packages(),
 
     # Packaging options.
-    include_package_data = True,
+    include_package_data=True,
 
     # Package dependencies.
-    install_requires = ['requests'],
+    install_requires=['requests'],
 
     # Metadata for PyPI.
-    author = 'Gengo',
-    author_email = 'api@gengo.com',
-    license = 'LGPL License',
-    url = 'http://github.com/myGengo/mygengo-python/tree/master',
-    keywords = 'gengo translation language api',
-    description = 'Official Python library for interfacing with the Gengo API.',
-    long_description = open('README.md').read(),
+    author='Gengo',
+    author_email='api@gengo.com',
+    license='LGPL License',
+    url='http://github.com/myGengo/mygengo-python/tree/master',
+    keywords='gengo translation language api',
+    description='Official Python library for interfacing with the Gengo API.',
+    long_description=open('README.md').read(),
     cmdclass={
         'pep8': Pep8Command,
     },
-    classifiers = [
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
